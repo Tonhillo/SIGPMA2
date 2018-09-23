54c15550327ffbff22dd95b289f477116b02cc58
Francisco Cruz <franpan_fp@hotmail.com>
2018-09-22 19:49:12 -0600

    Home.blade.php change to see if github works
    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,11 @@
     {{--<div class="row">--}}
     @desktop
     <div id="wrapper" style="width: 100%; height: 100%; overflow: auto; -webkit-overflow-scrolling: touch;">
+       Commit test
         <iframe  src="https://www.meteoblue.com/en/weather/widget/three/puntarenas_costa-rica_3622228?geoloc=fixed&nocurrent=0&noforecast=0&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=bright"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 100%;height: 598px; overflow: auto;" class="embed-responsive-item"></iframe>
     </div>
     @elsedesktop
-    <a class="weatherwidget-io" href="https://forecast7.com/en/9d22n83d34/puntarenas-province/" data-label_1="PUNTARENAS" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="pure" data-basecolor="rgba(255, 255, 255, 0)" >PUNTARENAS WEATHER</a>
+    <a class="weatherwidget-io" href="https://forecast7.com/en/9d22n83d34/puntarenas-province/" data-label_1="PUNTARENAS" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="pure" data-basecolor="rgba(100, 100, 255, 0)" >PUNTARENAS WEATHER</a>
     <script>
         !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
     </script>
